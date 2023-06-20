330e20af287cc58f3a3efc3d4a90dc8ccfe65850
Vanshika35 <112643736+Vanshika35@users.noreply.github.com>
2023-06-20 19:44:35 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    /*cout << "Hello world no idea how many times" << endl; 
+    cout << "Namaste Duniya" << endl;
+    int a = 123;
+    cout<< a << endl;
+
+    char b = 'v';
+    cout << b << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    float f = 1.2;
+    cout << f << endl;
+
+    double d = 1.23;
+    cout << d << endl;
+    // using size of function to check the size of 
+    cout << __SIZEOF_LONG_LONG__<<endl;
+    cout << sizeof(a) << endl;
+    string myText = "Coding Ninjas";
+    cout << myText << endl;
+    // overflow and underflow conditions in c++
+    // scopes in c++
+    // types of variables based on scopes in c++
+    // type casting
+    int f = 'a';
+    cout << f << endl;
+    char gh = 89;
+    cout << gh << endl;
+    char ch1 = 123456;
+    cout << ch1 << endl;
+    
+   unsigned int a = 112;
+   cout << a << endl;
+   // int/int = int and float/float = float but catch is int/float = float or float/int = float. 
+   int a = 2.0/4;
+   cout << a << endl;
+   cout << 2.0/5<< endl;*/
+   int a = 2;
+   int b = 3;
+   bool first = (a==b);
+   cout << first << endl;
+   
+
+
+
+}
